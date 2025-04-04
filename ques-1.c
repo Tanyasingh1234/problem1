429926e9f7ff7001c346b5ef93320abd520c852a
Tanyasingh1234 <tanyaparmar1234@gmail.com>
2025-04-04 21:26:28 +0530

    Add files via upload

---
+++ ques-1.c
@@ -0,0 +1,48 @@
+// // write a program to sort an array which consists the 0,1 and 2 without using sorting
+
+// #include<stdio.h>
+// int main()
+// {
+//     int n=7;
+//     int arr[] = { 0,2,1,0,1,2,1};
+
+//     // int n;
+//     // printf("enter the array of number=");
+//     // scanf("%d",&n);
+
+//     // int arr[n];
+//     // printf("Enter the elements of array:");
+//     // for(int i=0;i<n;i++)
+//     // {
+//     //     scanf("%d",&arr[n]);
+//     // }
+//     int count0,count1,count2;
+//     count0 = 0;
+//     count1 = 0;
+//     count2 = 0;
+//     for(int i = 0;i<n;i++)
+//     {
+//         if(arr[i]==0)
+//         {
+//             count0++;
+//         }
+//         else if(arr[i]==1)
+//         {
+//             count1++;
+//         }
+//         else{
+//             count2++;
+//         }
+//     }
+
+//     for(int i=0;i<count0;i++){
+//     printf("0");}
+//     for(int i=0;i<count1;i++){
+//     printf("1");}
+//     for(int i=0;i<count2;i++){
+//     printf("2");}
+
+//     return 0;
+// }
+
+
