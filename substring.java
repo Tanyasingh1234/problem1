0dd603594b1c34abf74d2e8eddc49f211ff7ac20
Tanyasingh1234 <tanyaparmar1234@gmail.com>
2025-04-11 22:27:17 +0530

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,17 @@
+// import java.util.Scanner;
+// public class substring{
+//     public static void main(String[] args){
+//         String s ="Tanya Singh";
+//         System.out.println(s.substring(0,5));
+
+//     }
+// }
+
+import java.util.Scanner;
+public class substring{
+    public static void main(String[] args){
+        String s ="Tanya Singh";
+        System.out.println(s.substring(6));
+
+    }
+}
